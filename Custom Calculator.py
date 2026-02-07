a3d78166f4f19f06f0340fac83b7f978140827c9
habibaMZF <habibasaeedgalal@gmail.com>
2026-02-07 17:09:22 +0200

    Add files via upload

---
+++ Custom Calculator.py
@@ -0,0 +1,12 @@
+def calculator(a, b, operation):
+    if operation == "add":
+        return a + b
+
+    if operation == "subtract":
+        return a - b
+
+    if operation == "multiply":
+        return a * b
+
+    if operation == "divide":
+        return a / b
